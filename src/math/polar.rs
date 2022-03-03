9687cd9e049de62e148838b5aa8b89727e2c4d7e
Jacob Davisson <davissonjb@gmail.com>
2022-03-02 23:49:46 -0600

    Added some basic implementations for Polar<T> struct.

--- src/math/polar.rs
+++ src/math/polar.rs
@@ -6,13 +6,41 @@
  * Modified:         1 MARCH 2022
  */
 
+use std::ops::{Add, Sub, Mul, Div};
+
 //
 //
 //
 //
 pub struct Polar<T>
 {
-    a: T,
-    b: T,
+    mag: T,
+    ang: T,
 }
 
+impl<T: Sub> Polar<T>
+{
+    fn conj(self) -> Self
+    {
+        Self {mag: self.mag, ang: self.ang}
+    }
+
+    fn Mag(self) -> T
+    {
+        self.mag
+    }
+
+    fn Ang(self) -> T
+    {
+        self.ang
+    }
+}
+
+/*impl<T: Add<Output = T>> Add for Polar<T>
+{
+    type Output = Self;
+    fn add(self, other: Self) -> Self
+    {
+        Self {}
+    }
+}*/
