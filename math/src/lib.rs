fd88b92cd2552ef6bdef6710be01855f7323e390
Jacob Davisson <davissonjb@gmail.com>
2024-02-23 21:13:55 -0600

    Significantly modified project structure to utilize Rust's workspace functionality. This simplifies varying aspects of development, and should improve the ability to modularize this project.

---
+++ math/src/lib.rs
@@ -0,0 +1,9 @@
+//
+// File: /math/src/lib.rs
+// Author: Jacob B. Davisson
+// Origination: 23 February 2024
+// Last Modified: 23 February 2024
+//
+
+pub mod polar;
+pub mod rect;
--- math/src/rect.rs
+++ math/src/rect.rs
@@ -7,15 +7,15 @@
  * Author:          Jacob B Davisson
  * Purpose:         Encapsulate functionality for rectangular data types
  * Origination:     1 MARCH 2022
- * Modified:        26 JUNE 2023
+ * Modified:        23 Februaty 2024
  */
+use std::ops::{Add, Div, Mul, Sub};
 
-use std::ops::{Add, Sub, Mul, Div};
+struct Scalar<T>(T);
 
-pub struct Rect<T>
-{
-   a: T,
-   b: T,
+pub struct Rect<T> {
+    a: T,
+    b: T,
 }
 
 /*
@@ -23,24 +23,26 @@ pub struct Rect<T>
  * ---------------------------------------------------------------------------------
  * ----------------------  Utility trait implementations  --------------------------
  * ---------------------------------------------------------------------------------
- * --------------------------------------------------------------------------------- 
+ * ---------------------------------------------------------------------------------
  */
 
-impl<T: Add<Output = T>> Add for Rect<T>
-{
-   type Output = Self;
-   fn add(self, other: Self) -> Self
-   {
-      Self {a: self.a + other.a, b: self.b + other.b}
-   }
+impl<T: Add<Output = T>> Add for Rect<T> {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            a: self.a + other.a,
+            b: self.b + other.b,
+        }
+    }
 }
 
-
-impl<T: Sub<Output=T>> Sub for Rect<T>
-{
-   type Output = Self;
-   fn sub(self, other: Self) -> Self
-   {
-      Self {a: self.a - other.a, b: self.b - other.b}
-   }
+impl<T: Sub<Output = T>> Sub for Rect<T> {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            a: self.a - other.a,
+            b: self.b - other.b,
+        }
+    }
 }
+
